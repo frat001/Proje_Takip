f7f8ff40e467e4af8e47ae7f093d6fef81e7a787
frat001 <firatozgurp@gmail.com>
2024-01-04 23:26:03 +0300

    Models

--- Proje_Takip/Models/Personel/PersonelBilgileri.cs
+++ Proje_Takip/Models/Personel/PersonelBilgileri.cs
@@ -14,34 +14,46 @@ namespace Proje_Takip.Models.Personel
 
         [DisplayName("E-Posta")]
         public string Email { get; set; }
+
         [DisplayName("ŞİFRE")]
-        [StringLength(25,ErrorMessage = "Maximum uzunluk 25 karakterden fazla olamaz")]
+        [StringLength(25, ErrorMessage = "Maximum uzunluk 25 karakterden fazla olamaz")]
         public string Sifre { get; set; }
+
         [DisplayName("YETKİ")]
         [StringLength(15, ErrorMessage = "Maximum uzunluk 15 karakterden fazla olamaz")]
-
         public string Yetki { get; set; }
+
         [DisplayName("AD SOYAD")]
         [StringLength(50, ErrorMessage = "Maximum uzunluk 50 karakterden fazla olamaz")]
-
         public string AdSoyad { get; set; }
-        [DisplayName("TC KİMLİK NO")]
 
-        public int TcNo { get; set; } 
-        [DisplayName("DEPARTMAN")]
+        [DisplayName("TC KİMLİK NO")]
+        [StringLength(11, ErrorMessage = "Maximum uzunluk 11 karakterden fazla olamaz")]
+        public string TcNo { get; set; }
+
+        [DisplayName("DEPARTMANI")]
         public string Departman { get; set; }
-        [DisplayName("GÖREV")]
-        public string Gorev { get; set; } 
+
+        [StringLength(25, ErrorMessage = "Maximum uzunluk 25 karakterden fazla olamaz")]
+        [DisplayName("GÖREVİ")]
+        public string Gorev { get; set; }
+
         [DisplayName("AÇIKLAMA")]
-        public string  PozisyonAciklama { get; set; }
+        public string PozisyonAciklama { get; set; }
+
         [DisplayName("TELEFON NUMARASI")]
-        public int TelNo { get; set; }   
+        [StringLength(15, ErrorMessage = "Maximum uzunluk 15 karakterden fazla olamaz")]
+        public int TelNo { get; set; }
+
         [DisplayName("ADRES BİLGİLERİ")]
         public string Adres { get; set; }
+
         [DisplayName("MEDENİ HAL")]
         public string MedeniHal { get; set; }
+
         [DisplayName("YAKIN BİLGİSİ")]
         public string YakinBilgisi { get; set; }
+
         [DisplayName("YAKIN TC KİMLİK NUMARASI")]
         public string YakinTc { get; set; }
         [DisplayName("YAKIN ADI SOYADI")]
