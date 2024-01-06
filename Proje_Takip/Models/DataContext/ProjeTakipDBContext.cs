d24bc75a27f9ea79f6b02c8dea4ea1a08d80cda5
frat001 <firatozgurp@gmail.com>
2024-01-06 23:45:10 +0300

    Db

---
+++ Proje_Takip/Models/DataContext/ProjeTakipDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Proje_Takip.Models.Personel;
+using Proje_Takip.Models.ProjeTakip;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Proje_Takip.Models.DataContext
+{
+    public class ProjeTakipDBContext : DbContext
+    {
+        public ProjeTakipDBContext() : base("ProjeTakipDB")
+        {
+
+
+        }
+
+        public DbSet<PersonelBilgileri> personelBilgileris { get; set; }
+
+        public DbSet<PersonelProjeleri> PersonelProjeleris { get; set; }
+    }
+}
